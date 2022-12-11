84f6083cbab4cd72d0fb180b7a5f2b293b375541
Dan Antennae <dan@redantennae.co.uk>
2022-12-11 13:01:24 +0000

    Add some initial tunes - menu only for now

---
+++ ModInit.lua
@@ -0,0 +1,19 @@
+-- ModInit for RedriverMusicPack
+
+local ModInit = {
+	Title = "Red Antennae music",
+
+	CreatedBy = "Red Antennae",
+	Version = "2.0",
+	PreBoot = false,
+}
+
+function ModInit:Init()
+
+end 
+
+function ModInit:DeInit()
+	
+end 
+
+return ModInit
